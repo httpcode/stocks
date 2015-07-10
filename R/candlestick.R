88cf2a3381e9acfe2f75df2c999724c3b58d260b
Daniel Pollard <daniel.pollard@oakton.com.au>
2015-07-10 14:19:19 +1000

    basic changes 4

--- R/candlestick.R
+++ R/candlestick.R
@@ -1,3 +1,12 @@
+#' Candlestick
+#' 
+#' Creates a candle stick
+#' 
+#' @param ticker stock ticker symbol. E.g. "GOOG". 
+#' @param from start date. Either string or date object.
+#' @param to end date. Either string or date object.
+#' @return candleChart object.
+#' @export
 candlestick <- function(ticker = "GOOG", from = "2013-01-01", to=Sys.time()){
   library(quantmod)
 
